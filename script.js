167f178f26f905fad75c6660b9a5607c98c66550
ai-by-humans <xtreme@ai-by-humans.com>
2023-08-26 01:27:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Global variables for form elements
+// Initialize global variables for each form element
 const nameSelect = document.getElementById("name");
 const ageSelect = document.getElementById("age");
 const bodySelect = document.getElementById("body");
@@ -8,22 +8,39 @@ const lovesSelect = document.getElementById("loves");
 const hatesSelect = document.getElementById("hates");
 const summarySection = document.getElementById("summary");
 
-// Function to add selected option to summary
+/**
+ * Function to add the selected option to the summary section.
+ * @param {string} selectId - The ID of the select element.
+ */
 function addSelected(selectId) {
+    // Get the value of the selected option
     const selectedOption = document.getElementById(selectId).value;
+    
+    // Check if an option is selected
     if (selectedOption) {
+        // Create a new paragraph element
         const summaryItem = document.createElement("p");
+        
+        // Set the text content to the selected option
         summaryItem.textContent = `${selectedOption}`;
+        
+        // Append the new paragraph to the summary section
         summarySection.appendChild(summaryItem);
     } else {
+        // Show an alert if no option is selected
         alert("Please select an option.");
     }
 }
 
-// Function to show summary on form submission
+/**
+ * Function to display the summary of selected options.
+ * Clears the previous summary before adding new items.
+ */
 function showSummary() {
-    summarySection.innerHTML = ""; // Clear previous summary
-    // Add selected options to the summary section
+    // Clear the previous summary
+    summarySection.innerHTML = "";
+    
+    // Add selected options for each form element to the summary
     addSelected("name");
     addSelected("age");
     addSelected("body");
@@ -31,5 +48,7 @@ function showSummary() {
     addSelected("personality");
     addSelected("loves");
     addSelected("hates");
-    return false; // Prevent form submission
+    
+    // Prevent the form from being submitted
+    return false;
 }
