73a62c58304bde18d986108759185c9de86f721e
ai-by-humans <xtreme@ai-by-humans.com>
2023-08-26 17:48:16 +0200

    0.02

--- script.js
+++ script.js
@@ -1,86 +1,96 @@
 // Initialize global variables for each form element
-const nameSelect = document.getElementById("name");
-const ageSelect = document.getElementById("age");
-const bodySelect = document.getElementById("body");
-const mindSelect = document.getElementById("mind");
-const personalitySelect = document.getElementById("personality");
-const lovesSelect = document.getElementById("loves");
-const hatesSelect = document.getElementById("hates");
+const nameDiv = document.getElementById("name");
+const ageDiv = document.getElementById("age");
+const bodyDiv = document.getElementById("body");
+const mindDiv = document.getElementById("mind");
+const personalityDiv = document.getElementById("personality");
+const lovesDiv = document.getElementById("loves");
+const hatesDiv = document.getElementById("hates");
 const summarySection = document.getElementById("summary");
 
-// Function to populate a select element from a .txt file
-async function populateSelect(selectElementId, txtFilePath) {
-  const selectElement = document.getElementById(selectElementId);
+// Function to populate a div element from a .txt file
+async function populateDiv(divElementId, txtFilePath) {
+  const divElement = document.getElementById(divElementId);
   
   // Fetch the .txt file
   const response = await fetch(txtFilePath);
   const text = await response.text();
   
-  // Split the text by lines and populate the select element
+  // Split the text by lines and populate the div element
   const lines = text.split('\n');
   lines.forEach(line => {
-    const option = document.createElement('option');
-    option.value = line;
-    option.text = line;
-    selectElement.add(option);
+    const word = document.createElement('span');
+    word.className = 'word-item';
+    word.textContent = line;
+    word.addEventListener('click', function() {
+      addSelected(divElementId, line);
+    });
+    divElement.appendChild(word);
   });
 }
 
-// Populate the select elements
-populateSelect('name', './data/name.txt');
-populateSelect('age', './data/age.txt');
-populateSelect('body', './data/body.txt');
-populateSelect('mind', './data/mind.txt');
-populateSelect('personality', './data/personality.txt');
-populateSelect('loves', './data/loves.txt');
-populateSelect('hates', './data/hates.txt');
+// Populate the div elements
+populateDiv('name', './data/name.txt');
+populateDiv('age', './data/age.txt');
+populateDiv('body', './data/body.txt');
+populateDiv('mind', './data/mind.txt');
+populateDiv('personality', './data/personality.txt');
+populateDiv('loves', './data/loves.txt');
+populateDiv('hates', './data/hates.txt');
 
-// Existing functions for summary
-// ... (addSelected and showSummary functions here)
-
-
-
-/**
- * Function to add the selected option to the summary section.
- * @param {string} selectId - The ID of the select element.
- */
-function addSelected(selectId) {
-    // Get the value of the selected option
-    const selectedOption = document.getElementById(selectId).value;
-    
-    // Check if an option is selected
-    if (selectedOption) {
-        // Create a new paragraph element
-        const summaryItem = document.createElement("p");
-        
-        // Set the text content to the selected option
-        summaryItem.textContent = `${selectedOption}`;
-        
-        // Append the new paragraph to the summary section
-        summarySection.appendChild(summaryItem);
-    } else {
-        // Show an alert if no option is selected
-        alert("Please select an option.");
-    }
+// Function to add the clicked word to the summary section
+function addSelected(divId, word) {
+  const summaryItem = document.createElement("p");
+  summaryItem.textContent = word;
+  summarySection.appendChild(summaryItem);
 }
 
-/**
- * Function to display the summary of selected options.
- * Clears the previous summary before adding new items.
- */
+// Function to display the summary of clicked words
 function showSummary() {
-    // Clear the previous summary
-    summarySection.innerHTML = "";
-    
-    // Add selected options for each form element to the summary
-    addSelected("name");
-    addSelected("age");
-    addSelected("body");
-    addSelected("mind");
-    addSelected("personality");
-    addSelected("loves");
-    addSelected("hates");
-    
-    // Prevent t the form from being submitted
-    return false;
+  // Clear the previous summary
+  summarySection.innerHTML = "";
+  
+  // Prevent the form from being submitted
+  return false;
 }
+
+let lastX = 0;
+let lastY = 0;
+
+// Function to dynamically scale and move words based on mouse movement and proximity
+document.addEventListener('mousemove', function(event) {
+  const x = (event.clientX + lastX) / 2;
+  const y = (event.clientY + lastY) / 2;
+
+  lastX = x;
+  lastY = y;
+
+  document.querySelectorAll('.word-item').forEach(word1 => {
+    let dxSum = 0;
+    let dySum = 0;
+
+    // Calculate the effect of other words on this word
+    document.querySelectorAll('.word-item').forEach(word2 => {
+      if (word1 !== word2) {
+        const rect1 = word1.getBoundingClientRect();
+        const rect2 = word2.getBoundingClientRect();
+        const dx = rect1.left - rect2.left;
+        const dy = rect1.top - rect2.top;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < 50) {
+          dxSum += dx / distance;
+          dySum += dy / distance;
+        }
+      }
+    });
+
+    const rect = word1.getBoundingClientRect();
+    const dx = rect.left + rect.width / 2 - x;
+    const dy = rect.top + rect.height / 2 - y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const scale = Math.min(20 / distance, 1.2);
+
+    word1.style.transform = `translate(${dxSum * 5}px, ${dySum * 5}px) scale(${scale})`;
+  });
+});
